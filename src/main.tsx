819f79c4426fdc7e7de1e85c95812c7444dd62c8
Guilherme Mazutti <mazuttimail@gmail.com>
2024-02-09 15:46:38 -0300

    Finaliza projeto notes-nlw

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app'
-import { Toaster } from 'sonner'
+
 import './index.css'
+import { Toaster } from 'sonner'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
